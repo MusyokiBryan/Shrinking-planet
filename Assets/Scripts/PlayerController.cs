9d495e5a3b0a2226f3a4ba7fac397e37847a3b04
Brian Musyoki <musyokibr@gmail.com>
2023-01-14 13:01:30 +0300

    Initial commit
    Adding mobile controls

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,30 +2,33 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-	public float moveSpeed = 10f;
-	public float rotationSpeed = 10f;
+    public float moveSpeed = 10f;
+    public float rotationSpeed = 10f;
+    public float touchSensitivity = 100f;
 
-	private float rotation;
-	private Rigidbody rb;
+    private float rotation;
+    private Rigidbody rb;
 
-	void Start ()
-	{
-		rb = GetComponent<Rigidbody>();
-	}
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
-	void Update ()
-	{
-		rotation = Input.GetAxisRaw("Horizontal");
-	}
+    void Update ()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            rotation = touch.position.x / touchSensitivity;
+        }
+    }
 
-	void FixedUpdate ()
-	{
-		rb.MovePosition(rb.position + transform.forward * moveSpeed * Time.fixedDeltaTime);
-		Vector3 yRotation = Vector3.up * rotation * rotationSpeed * Time.fixedDeltaTime;
-		Quaternion deltaRotation = Quaternion.Euler(yRotation);
-		Quaternion targetRotation = rb.rotation * deltaRotation;
-		rb.MoveRotation(Quaternion.Slerp(rb.rotation, targetRotation, 50f * Time.deltaTime));
-		//transform.Rotate(0f, rotation * rotationSpeed * Time.fixedDeltaTime, 0f, Space.Self);
-	}
-
+    void FixedUpdate ()
+    {
+        rb.MovePosition(rb.position + transform.forward * moveSpeed * Time.fixedDeltaTime);
+        Vector3 yRotation = Vector3.up * rotation * rotationSpeed * Time.fixedDeltaTime;
+        Quaternion deltaRotation = Quaternion.Euler(yRotation);
+        Quaternion targetRotation = rb.rotation * deltaRotation;
+        rb.MoveRotation(Quaternion.Slerp(rb.rotation, targetRotation, 50f * Time.deltaTime));
+    }
 }
