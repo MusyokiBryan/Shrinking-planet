5ec94a117ced75745dd11b205f7fe58a92b4849c
Brian Musyoki <musyokibr@gmail.com>
2023-01-15 17:47:13 +0300

    Working build
    Adding:
    -Touch controls
    -FPS counter
    -Optimizing the game to a smaller size
    -Rebuilding for better fps count

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -1,21 +1,21 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
-public class GameOver : MonoBehaviour {
+public class GameOver : MonoBehaviour, IPointerDownHandler
+{
+public void OnPointerDown(PointerEventData eventData)
+{
+GameManager.instance.Restart();
+}
 
-	void Update ()
-	{
-		if (Input.GetButtonDown("Jump"))
-		{
-			GameManager.instance.Restart();
-		}
+void Update()
+{
+    GetComponent<RectTransform>().localScale = Vector3.one * Planet.Size;
+}
 
-		GetComponent<RectTransform>().localScale = Vector3.one * Planet.Size;
-	}
-
-	public void Menu ()
-	{
-		SceneManager.LoadScene("Menu");
-	}
-
+public void Menu()
+{
+    SceneManager.LoadScene("Menu");
+}
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,33 +2,32 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-    public float moveSpeed = 10f;
-    public float rotationSpeed = 10f;
-    public float touchSensitivity = 100f;
+	public float moveSpeed = 10f;
+	public float rotationSpeed = 10f;
 
-    private float rotation;
-    private Rigidbody rb;
+	private float rotation;
+	private Rigidbody rb;
 
-    void Start ()
-    {
-        rb = GetComponent<Rigidbody>();
-    }
+	public Joystick joystick;
 
-    void Update ()
-    {
-        if (Input.touchCount > 0)
-        {
-            Touch touch = Input.GetTouch(0);
-            rotation = touch.position.x / touchSensitivity;
-        }
-    }
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
 
-    void FixedUpdate ()
-    {
-        rb.MovePosition(rb.position + transform.forward * moveSpeed * Time.fixedDeltaTime);
-        Vector3 yRotation = Vector3.up * rotation * rotationSpeed * Time.fixedDeltaTime;
-        Quaternion deltaRotation = Quaternion.Euler(yRotation);
-        Quaternion targetRotation = rb.rotation * deltaRotation;
-        rb.MoveRotation(Quaternion.Slerp(rb.rotation, targetRotation, 50f * Time.deltaTime));
-    }
+	void Update ()
+	{
+		rotation = joystick.Horizontal;
+	}
+
+	void FixedUpdate ()
+	{
+		rb.MovePosition(rb.position + transform.forward * moveSpeed * Time.fixedDeltaTime);
+		Vector3 yRotation = Vector3.up * rotation * rotationSpeed * Time.fixedDeltaTime;
+		Quaternion deltaRotation = Quaternion.Euler(yRotation);
+		Quaternion targetRotation = rb.rotation * deltaRotation;
+		rb.MoveRotation(Quaternion.Slerp(rb.rotation, targetRotation, 50f * Time.deltaTime));
+		
+	}
+
 }
